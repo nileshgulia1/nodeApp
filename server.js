749b8874adba09a3db01f98fdad5c37d30536d49
nilesh <nileshgulia@gmail.com>
2017-11-07 00:34:05 +0530

    added ig api

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var ig = require('instagram-node').instagram();
 
+// configure instagram app with your access_token
+ig.use({
+    // get access token here: http://instagram.pixelunion.net/
+    access_token: 'MY_ACCESS_TOKEN',
+    });
+
 
 // tell node where to look for site resources
 app.use(express.static(__dirname + '/public'));
@@ -9,9 +15,12 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-
-res.render('pages/index');
-});
+    // use the instagram package to get popular media
+    ig.user_self_media_recent(function(err, medias, pagination, remaining, limit) {
+    // render the home page and pass in the popular images
+    res.render('pages/index', { grams: medias });
+    });
+    });
 // START THE SERVER
 
 app.listen(8080);
